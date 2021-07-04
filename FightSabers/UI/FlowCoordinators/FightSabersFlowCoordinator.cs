6de746e94a1479ae5e2b78f721705ab108d8d584
Eris <erisapps@outlook.be>
2021-07-05 01:52:45 +0200

    Injected HomePageController and BottomPageController in FlowCoordinator

--- FightSabers/UI/FlowCoordinators/FightSabersFlowCoordinator.cs
+++ FightSabers/UI/FlowCoordinators/FightSabersFlowCoordinator.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.ViewControllers;
 using FightSabers.UI.Controllers;
 using HMUI;
+using Zenject;
 
 namespace FightSabers.UI.FlowCoordinators
 {
 	internal class FightSabersFlowCoordinator : FlowCoordinator
 	{
-		private BottomPageController _bottomController;
+		private HomePageController _homePageController = null!;
+		private BottomPageController _bottomPageController = null!;
 
 		public enum PageStatus
 		{
@@ -22,6 +25,13 @@ namespace FightSabers.UI.FlowCoordinators
 
 		public PageStatus CurrentPageStatus { get; private set; }
 
+		[Inject]
+		internal void Construct(HomePageController homePageController, BottomPageController bottomPageController)
+		{
+			_homePageController = homePageController;
+			_bottomPageController = bottomPageController;
+		}
+
 		protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
 		{
 			if (!addedToHierarchy)
@@ -29,11 +39,8 @@ namespace FightSabers.UI.FlowCoordinators
 				return;
 			}
 
-			CurrentPageStatus = PageStatus.Home;
-			var homeController = BeatSaberUI.CreateViewController<HomePageController>();
-			_bottomController = BeatSaberUI.CreateViewController<BottomPageController>();
-			_bottomController.FlowCoordinatorOwner = homeController.flowCoordinatorOwner = this;
-			ProvideInitialViewControllers(homeController, null, null, _bottomController);
+			_bottomPageController.FlowCoordinatorOwner = this;
+			ActivatePage(PageStatus.Home);
 		}
 
 		public void ActivatePage(PageStatus status)
@@ -43,41 +50,41 @@ namespace FightSabers.UI.FlowCoordinators
 				return;
 			}
 
-			FightSabersViewController controller;
+			BSMLAutomaticViewController controller;
 			switch (status)
 			{
 				case PageStatus.Home:
-					controller = BeatSaberUI.CreateViewController<HomePageController>();
+					controller = _homePageController;
 					ReplaceTopViewController(controller);
 					SetLeftScreenViewController(null, ViewController.AnimationType.Out);
 					SetRightScreenViewController(null, ViewController.AnimationType.Out);
-					ProvideInitialViewControllers(controller, null, null, _bottomController);
+					ProvideInitialViewControllers(controller, null, null, _bottomPageController);
 					break;
 				case PageStatus.Skills:
 					controller = BeatSaberUI.CreateViewController<SkillTreePageController>();
 					ReplaceTopViewController(controller);
 					SetLeftScreenViewController(null, ViewController.AnimationType.Out);
 					SetRightScreenViewController(null, ViewController.AnimationType.Out);
-					ProvideInitialViewControllers(controller, null, null, _bottomController);
+					ProvideInitialViewControllers(controller, null, null, _bottomPageController);
 					break;
 				case PageStatus.Profile:
 					controller = BeatSaberUI.CreateViewController<ProfilePageController>();
 					ReplaceTopViewController(controller);
 					SetLeftScreenViewController(null, ViewController.AnimationType.Out);
 					SetRightScreenViewController(null, ViewController.AnimationType.Out);
-					ProvideInitialViewControllers(controller, null, null, _bottomController);
+					ProvideInitialViewControllers(controller, null, null, _bottomPageController);
 					break;
 				case PageStatus.Quests:
 					controller = BeatSaberUI.CreateViewController<QuestPickerPageController>();
 					ReplaceTopViewController(controller);
 					SetRightScreenViewController(null, ViewController.AnimationType.Out);
-					ProvideInitialViewControllers(controller, null, null, _bottomController);
+					ProvideInitialViewControllers(controller, null, null, _bottomPageController);
 					SetLeftScreenViewController(BeatSaberUI.CreateViewController<CurrentQuestPageController>(), ViewController.AnimationType.In);
 					break;
 				case PageStatus.Statistics:
 					controller = BeatSaberUI.CreateViewController<CharacterStatsPageController>();
 					ReplaceTopViewController(controller);
-					ProvideInitialViewControllers(controller, null, null, _bottomController);
+					ProvideInitialViewControllers(controller, null, null, _bottomPageController);
 					SetLeftScreenViewController(BeatSaberUI.CreateViewController<MonsterInfoPageController>(), ViewController.AnimationType.In);
 					SetRightScreenViewController(BeatSaberUI.CreateViewController<ModifierStatsPageController>(), ViewController.AnimationType.In);
 					break;
@@ -85,7 +92,6 @@ namespace FightSabers.UI.FlowCoordinators
 					throw new ArgumentOutOfRangeException(nameof(status), status, null);
 			}
 
-			controller.flowCoordinatorOwner = this;
 			CurrentPageStatus = status;
 		}
 	}
