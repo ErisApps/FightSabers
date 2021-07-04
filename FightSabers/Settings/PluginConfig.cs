304ced79b106d74fc406bbf82de37be205a2d194
Eris <erisapps@outlook.be>
2021-07-04 17:24:53 +0200

    Slightly reworked settings class

--- FightSabers/Settings/PluginConfig.cs
+++ FightSabers/Settings/PluginConfig.cs
@@ -1,12 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
+using IPA.Config.Stores.Attributes;
+using SiraUtil.Converters;
+using UnityEngine;
 
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
 namespace FightSabers.Settings
 {
-    internal class PluginConfig
-    {
-        public bool RegenerateConfig = true;
-        public bool Enabled = true;
-        public Vector3 FSPanelPosition = new Vector3(0, 3.85f, 2.4f);
-        public Vector3 FSPanelRotation = new Vector3(-15, 0, 0);
-    }
+	internal class PluginConfig
+	{
+		public event EventHandler? ConfigChanged;
+
+		public virtual bool Enabled { get; set; } = true;
+
+		[UseConverter(typeof(Vector3Converter))]
+		public virtual Vector3 PanelPosition { get; set; } = new(0, 3.85f, 2.4f);
+
+		[UseConverter(typeof(Vector3Converter))]
+		public virtual Vector3 PanelRotation { get; set; } = new(-15, 0, 0);
+
+		public virtual void OnReload()
+		{
+			// This is called whenever the config file is reloaded from disk.
+			// Use it to tell all of your systems that something has changed.
+
+			// This is called off of the main thread, and is not safe to interact with Unity in
+			ConfigChanged?.Invoke(this, EventArgs.Empty);
+		}
+
+		public virtual void Changed()
+		{
+			// this is called whenever one of the virtual properties is changed
+			// can be called to signal that the content has been changed
+		}
+
+		public virtual IDisposable ChangeTransaction => null!;
+	}
 }
